b220f6e8e446323789148bca053af181284754a9
khusil <khushilmanji@yahoo.com>
2023-03-27 16:38:19 -0400

    added if else statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,26 @@
-$(document).ready(funtion(){
+$(document).ready(funtion() {
   var today = dayjs().format('dddd MMMM D');
   var date = document.getElementById('currentDay')
   date.textcontent = today;
-  
+
+  var hour = dayjs().hour();
+  $('.time-block').each(funtion()) {
+    var blockNum = parseInt($(this).attr('id').split('hour')[1]);
+    if (blockNum < currentHour) {
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+      $(this).addClass('past');
+      $(this).children('.description').addClass('text-light');
+    } else if (blockNum === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  }
+
 
 })
